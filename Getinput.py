b16b0e9796638771ede676660fb50fe2c238c767
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-03-19 14:48:51 +0700

    update

---
+++ Getinput.py
@@ -0,0 +1,17 @@
+# Mempelajari cara dasar mendapatkan nilai dari iput 
+
+# data yang ditampilkan otomatis berupa string
+huruf = input ("masukan huruf =")
+print (huruf, "tipe ",type(huruf))
+
+# get input integer / float
+
+#angka = int ( input ("masukan angka ="))
+angka = float (input ("masukan angka ="))
+print (angka, "tipe data",type(angka))
+
+
+# get input boolean
+biner = bool (int(input("masukan biner ="))) #input dicasting ke integer dulu lalu dicasting ke boolean
+print ("biner bernilai =",biner, "tipe data",type(biner))
+

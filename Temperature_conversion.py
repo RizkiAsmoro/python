db4d7cdb0abfadeabe69907615617b1daa180861
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-03-22 16:10:56 +0700

    first commit

---
+++ Temperature_conversion.py
@@ -0,0 +1,22 @@
+# Conversi suhu sederhana
+print ("\n Temperature Conversion from Celcius\n")
+
+celcius = float(input('input celcius temperature = '))
+#print ('temperature in celcius is = ',celcius)
+
+reamur = (4/5) * celcius #convert celcius to reamur
+fahrenheit = ((9/5) * celcius) + 32 #convert celcius to fahrenheit
+kelvin = celcius + 273.15 #convert celcius to kelvin
+
+
+print (celcius,'celcius = ',reamur,'reamur')
+print (celcius,'celcius = ',fahrenheit,'fahrenheit')
+print (celcius,'celcius = ',kelvin,'kelvin')
+
+
+
+
+
+
+
+

8c770f83eeb80bbdaf5aea47855f2f8b92aa09f6
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-03-24 15:57:18 +0700

    data type intro

--- Main.py
+++ Main.py
@@ -1,26 +1,21 @@
-# Comment
-"""Notes : Untuk memindahkan baris code keatas atau kebawah dapat dilakukan dengan 
-meletakan kursor paling kiri baris code lalu 
-menekan tombol ALT + arah atas atau bawah"""
+'''
+DATA TYPE
+'''
 
-"""Notes : untuk melakukan copy pada suatu baris code dapat dilakukan dengan
-meletakan kursor paling kiri baris code lalu tekan ALT + SIFT + arah bawah/atas"""
+data_integer = 1234567890 #numbers
+data_float = 13.7 #numbers with coma
+data_string = "rizkiasmoro@gmail.com"
+data_boolean = True #value true and false (boolean)
+data_complex = complex(5,6) #complex number
 
-# jenis-jenis tipe data
-data_integer = 1234567890 #angka tanpa koma
-data_float = 13.7 #angka dengan koma
-data_string = "rizkiasmoro@gmail.com" #huruf atau simbol, jika ada angka maka angka tsb dikenali sebagai string bukan integer
-data_boolean = True #tipe data biner true atau false (boolean)
-data_complex = complex(5,6) #bilangan kompleks
-
-#import tipe data dari C untuk menggunakan tipe data dari bahasa C. misal c_double
+#import data type from C to use data type from C like c_double
 from ctypes import c_double
 data_c_double = c_double(10.8)
-print ("Belajar mengenal tipe data")
-print ("data :",data_integer,"bertipe data",type (data_integer))
-print ("data :",data_float,"bertipe data",type (data_float))
-print ("data :",data_string,"bertipe data",type (data_string))
-print ("data :",data_boolean,"bertipe data",type(data_boolean))
-print ("data :",data_complex,"bertipe data",type (data_complex))
-print ("data :",data_c_double,"bertipe data",type (data_c_double))
+print ("Data type introduction")
+print ("data :",data_integer,"data type",type (data_integer))
+print ("data :",data_float,"data type",type (data_float))
+print ("data :",data_string,"data type",type (data_string))
+print ("data :",data_boolean,"data type",type(data_boolean))
+print ("data :",data_complex,"data type",type (data_complex))
+print ("data :",data_c_double,"data type",type (data_c_double))
 

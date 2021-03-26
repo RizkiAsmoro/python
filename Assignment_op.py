c34f6dce184e21464f525ad7ad61287ecbed4b0e
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-03-26 15:54:10 +0700

    first

---
+++ Assignment_op.py
@@ -0,0 +1,18 @@
+'''
+Assignment Operation
+'''
+
+x = 10
+print ("x Valus is =",x)
+
+x +=2
+print ('if x +=2, x value tobe :',x) # its mean x + 2
+
+x -=4
+print ('then x -=4, x value tobe :',x) # its mean x - 4
+
+x *=2
+print ('then x *=2, x value tobe :',x) # its mean x * 2
+
+x /=4
+print ('then x /=4, x value tobe :',x) # its mean x / 2

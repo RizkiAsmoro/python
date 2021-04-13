f4fee0f6c62c8737ed7bece2fffa1416c90b9c25
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-13 14:29:31 +0700

    change add remove of list

--- ChangeAddRemove_list.py
+++ ChangeAddRemove_list.py
@@ -23,11 +23,44 @@ print("->change item range 1-2:\n",crew)
 crew = ["nami","robin","hancock","reiju","monet"]
 crew[1:3] = ["stussy"]#new values item will be insert specify, remaining list in specify range will remove
 print("->change item range 1-2:\n",crew)
-#Sample 4 (insert). Insert new item list without replacing any of existing list
-crew = ["nami","robin","hancock","reiju","monet"]
-crew.insert(2,"shirahoshi") 
-print("insert to index 2 :",crew)
 
 print("===================ADD==================")
+#append()
+crew = ["nami","robin","hancock","reiju","monet"]
+crew.append("vivi")#add item to the end list
+print(crew)
+#insert() 
+crew = ["nami","robin","hancock","reiju","monet"]
+crew.insert(2,"shirahoshi")#Insert new item list without replacing any of existing list 
+print("insert to index 2 :",crew)
+#extend()
+crew = ["nami","robin","hancock","reiju","monet"]
+navy = ["garp","sengoku","smoker"]
+crew.extend(navy)#to append element from another list
+print(crew)
+
+print("===================REMOVE==================")
+#remove specify item
+crew = ["nami","robin","hancock","reiju","monet"]
+crew.remove("monet")
+print(crew)
+#pop() to remove specify index
+crew = ["nami","robin","hancock","reiju","monet"]
+navy = ["garp","sengoku","smoker"]
+crew.pop(-2)
+navy.pop() #remove the last list
+print(crew)
+print(navy)
+#del alse can remove specify index
+#if using 'del navy' it will error navy is not define its mean succesfully deleted
+crew = ["nami","robin","hancock","reiju","monet"]
+navy = ["garp","sengoku","smoker"]
+del crew[3]
+print(crew)
+#clear() used for empies the list
+crew = ["nami","robin","hancock","reiju","monet"]
+crew.clear()
+print(crew)
+
 
 

760941b137b6e01314ce4b638880cf5d4fec3ba4
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-13 15:32:33 +0700

    list

--- List.py
+++ List.py
@@ -25,3 +25,64 @@ print("->List can be any data type:",players)
 players = list(("ronaldo", "messi", "neymar", "mbape", "ronaldo", "haland"))
 print("->",players)
 
+print("=================SORT=================")
+#Sort() to sort alphanumerically & ascending by default
+players = ["ronaldo", "messi", "neymar", "mbape", "ronaldo", "haland"]
+players_no = [7,10,11,9,7,13]
+players.sort()#sort by ascending
+players_no.sort()
+print(players) 
+print(players_no)
+#sort descending
+players.sort(reverse=True)
+players_no.sort(reverse=True)
+print(players) 
+print(players_no)
+#case sensitive sort
+players = ["Ronaldo", "messi", "neymar", "mbape", "haland"]
+players.sort()#starting from that beginning with capital
+print(players)
+players = ["Ronaldo", "messi", "neymar", "mbape", "haland"]
+players.sort(key=str.lower)#lowercase came first
+print(players)
+#reverse() to reverse item list
+players = ["Ronaldo", "messi", "neymar", "mbape", "haland"]
+players.reverse()
+print(players)
+#sort the list based on how close the number is to 9
+def myfunc(n):
+    return abs(n - 9) #count the difference
+players_no = [7,10,11,9,7,13]
+players_no.sort(key = myfunc)
+print(players_no)
+
+print("=================COPY=================")
+'''
+can't simply like data1 = data2, because data2 will be reference to data1 
+and, if data1 list changed then data2 will changed(dependencies)
+'''
+data1 = ["luffy", "zoro", "sanji", "usop"]
+crew1 = data1.copy() #using method copy()
+print(crew1)
+
+data2 = ["franky", "jimbei", "chopper"]
+crew2 = list(data2)# using method list()
+print(crew2)
+
+print("=================JOIN=================")
+#using operator +
+data1 = ["luffy", "zoro", "sanji", "usop"]
+data2 = ["franky", "jimbei", "chopper"]
+data3 = data1 + data2
+print("data1 + data 2 =\n",data3)
+#using append
+data1 = ["luffy", "zoro", "sanji", "usop"]
+data2 = ["franky", "jimbei", "chopper"]
+for x in data2:
+    data1.append(x)
+print("data1 append data2 =\n",data1)
+#using extend()
+data1 = ["luffy", "zoro", "sanji", "usop"]
+data2 = ["franky", "jimbei", "chopper"]
+data1.extend(data2)
+print(data1)

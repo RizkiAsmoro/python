963dd6d0de950ee882775f767825c77facbae5a9
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-19 15:28:21 +0700

    continue pass

--- Looping_2.py
+++ Looping_2.py
@@ -1,17 +1,18 @@
 '''
-FOR Loop Else
+FOR,Else Loop
+Continue, pass
 '''
 # print range number
 for i in range(0,5): 
     print(i)
 
-print(20*"=")
+print(20*"=","range increment")
 for i in range (10,30,5): #range 10 to 30, increment 5
     print(i)
 
-#
-print(20*"=")
-number = int(input("insert number 1 to 5:"))
+# For Else
+print(20*"=","for else")
+number = 3
 for i in range (1,6):
     print(i) # print range number 1 to 5
     if i is number:
@@ -19,3 +20,30 @@ for i in range (1,6):
         break # 'break' to exit from 'if' condition 
 else: # 'else' still inside 'for loop' condition
     print("number not found") 
+
+
+#continue
+print(20*"=","continue")
+for i in range(1,6):
+    if i is 3:
+        print(i)
+        continue
+        print("check number")
+    print("current number is ",i)
+    if i is 3:
+        print("number",3,"was found")
+else:
+    print("loop ended")
+
+#pass
+print(20*"=","pass")
+for i in range(1,6):
+    if i is 3:
+        print("i number is ",i)
+        pass
+        print("check number")
+    print("current number is ",i)
+    if i is 3:
+        print("number",3,"was found")
+else:
+    print("loop ended")

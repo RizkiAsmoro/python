f627b253599451ce7d26bafbbb11a337b30bf753
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-20 15:04:39 +0700

    range,break,continue,pass

--- Looping_2.py
+++ Looping_2.py
@@ -3,6 +3,7 @@ FOR,Else Loop
 Continue, pass
 '''
 # print range number
+print(20*"=","RANGE")
 for i in range(0,5): 
     print(i)
 
@@ -11,7 +12,7 @@ for i in range (10,30,5): #range 10 to 30, increment 5
     print(i)
 
 # For Else
-print(20*"=","for else")
+print(20*"=","FOR ELSE")
 number = 3
 for i in range (1,6):
     print(i) # print range number 1 to 5
@@ -22,28 +23,38 @@ else: # 'else' still inside 'for loop' condition
     print("number not found") 
 
 
-#continue
-print(20*"=","continue")
-for i in range(1,6):
-    if i is 3:
-        print(i)
-        continue
-        print("check number")
-    print("current number is ",i)
-    if i is 3:
-        print("number",3,"was found")
+#BREAK
+print(20*"=","BREAK")
+for y in range(1,6):
+    if y is 2:
+        print("number found :",2)
+        break # when the number was found, break will ended proccess of loop and out of loop
+        print("check")
+    print("number",y)
 else:
-    print("loop ended")
+    print("end of loop")
+print("out of loop")
+
+#continue
+print(20*"=","CONTINUE")
+for x in range(1,6):
+    if x is 3:
+        print("number found",3)
+        continue # when the number has been found, continue the looping proccess until end of range then out of loop
+        print("check")
+    print("number",x)
+else:
+    print("end of loop")
+print("out of loop")
 
 #pass
-print(20*"=","pass")
-for i in range(1,6):
-    if i is 3:
-        print("i number is ",i)
-        pass
-        print("check number")
-    print("current number is ",i)
-    if i is 3:
-        print("number",3,"was found")
+print(20*"=","PASS")
+for z in range(1,6):
+    if z is 2:
+        print("number found",2)
+        pass # when number was found, pass will print check then continue procces until loop proccess ended
+        print("check")
+    print("number",z)
 else:
-    print("loop ended")
+    print("end of loop")
+print("out of loop")

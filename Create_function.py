3a5edc0f23706fdb4509765ef801e30cacff3a6c
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-26 11:29:03 +0700

    create function

---
+++ Create_function.py
@@ -0,0 +1,16 @@
+'''
+How to Create Function 
+'''
+#define function
+def function():
+    print("Have a nice day")
+#call function
+function()
+
+print(20*"=","perimeter fuction")
+#define dunction and create formula
+def perimeter_square(a):
+    p = a * 4
+    print("perimeter of a square is :",p)
+function() # call the function inside the other function
+perimeter_square(2) # set input value 

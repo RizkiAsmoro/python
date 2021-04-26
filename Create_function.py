b498fd63e6233bdfadbe8c4c5cca33442df0becf
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-26 11:39:28 +0700

    how to create function

--- Create_function.py
+++ Create_function.py
@@ -7,10 +7,19 @@ def function():
 #call function
 function()
 
-print(20*"=","perimeter fuction")
+print(20*"=","perimeter square fuction")
 #define dunction and create formula
 def perimeter_square(a):
     p = a * 4
     print("perimeter of a square is :",p)
 function() # call the function inside the other function
 perimeter_square(2) # set input value 
+
+print(20*"=","perimeter rectangle fuction")
+def perimeter_rectangle():
+    l = 3
+    w = 5
+    p = 2*(l+w)
+    print("if length is ",l,"and width is ",w)
+    print("perimeter rectangle is :",p)
+perimeter_rectangle()

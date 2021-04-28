29142ff54ccfcf88f1c7e914b2296be45243513d
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-28 12:03:49 +0700

    update

--- Return_value.py
+++ Return_value.py
@@ -14,6 +14,10 @@ def sum(value1, value2):
 a = multiplication(2, 5)
 b = sum(3, 4)
 c = a - b
+d = multiplication(a, b)
+e = sum(a, multiplication(a, b))
 print("a =",a) 
 print("b =",b) 
 print("a - b =",c) #the function can be used again and print
+print("a x b =",d)
+print("sum value of a and result multiplication between a and b =",e)

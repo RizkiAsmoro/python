7a1d5e7bfacfab2d1deafee56e8ed17e1facd0d2
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-28 11:07:09 +0700

    function return value

--- Return_value.py
+++ Return_value.py
@@ -1,8 +1,19 @@
 '''
 Function Return Value
+so that the function can be used again
 '''
 def SquarePerimeter(argument):
     total = argument * 4
-    print("value perimeter square from side :",argument,"is", total)
+    print("-a box with each side of",argument,"cm then the perimeter is", total)
+    return total
+a = SquarePerimeter(2)
+print("-Value",a) #the function can be used again and print
 
-SquarePerimeter(4)
+print(30*"=")
+
+def multiplication(value1,value2):
+    total : value1 * value2
+    print(value1,"x",value2,"=",total)
+    return total 
+x = multiplication(4,3)
+print(x)

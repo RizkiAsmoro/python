257e9c8798771aa79ba1dc9da8dda463b6f2041f
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-04-30 13:22:29 +0700

    updates

---
+++ Stacks_queues.py
@@ -0,0 +1,33 @@
+'''
+Stacks : Last-in-First-Out (LIFO) principle
+Queues : First-in-First-Out (FIFO) principle
+'''
+from collections import deque
+
+#Stacks
+print(25*"=","STACKS")
+numbers = [1,2,3,4,5,6]
+print("-list",numbers)
+
+numbers.append(7)
+print("-add 7 in list",numbers)
+
+numbers.append(8)
+print("-add 8 in list", numbers)
+
+numbers.pop()
+print("-Last in - first out",numbers)
+
+#Queues
+print(25*"=","QUEUES")
+numbers = deque([1,2,3,4,5,6])
+print("-list",numbers)
+
+numbers.append(7)
+print("-add 7 in list",numbers)
+
+numbers.append(8)
+print("-add 8 in list", numbers)
+
+numbers.popleft()
+print("-First in - First out",numbers)

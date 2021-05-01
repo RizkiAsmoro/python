ff06bc20710281778d67fca00a1ed49f15066a83
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-05-01 15:36:16 +0700

    update

--- For_loop3.py
+++ For_loop3.py
@@ -10,7 +10,7 @@ players = ['ronaldo',
           'hazard']
 clubs = ['juventus',
          'barcelona',
-         'dortmund',
+         'borusia dortmund',
          'PSG',
          'bayern munic',
          'real madrid']
@@ -19,3 +19,34 @@ clubs = ['juventus',
 #indexing using enumerate
 for i, name in enumerate(players):
     print(i, name) # indexing start from 0
+
+# ZIP
+# each literacy must be paired sequentially
+for x,y in zip(players, clubs):   
+    print(x,'from',y)
+
+# SETS
+print("="*25,"sorting the SETS")
+crews = {"luffy", "zoro", "sanji", "usop","brook","franky", "jimbei", "chopper","nami","robin"}
+for x in sorted(crews):
+    print(x)
+
+# DICTIONARIES
+print("="*25,"DICTIONARIES")
+pirates = {"luffy":"captain",
+           "usop":"sniper",
+           "nami":"navigator",
+           "brook":"musician"}
+print("---get the keys---")
+for y in pirates.keys(): #will get the keys only
+    print(y)
+print("---get the values---")
+for a in pirates.values(): #will get the value only
+    print(a)
+print("---get the items---")
+for i in pirates.items(): #will get the items of the data
+    print(i)
+
+
+
+

bc0e8db3e4c8dcb9c7420e4d58fcc72cc4bc9820
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-05-02 15:57:36 +0700

    update class and method

--- ClassAndMethod.py
+++ ClassAndMethod.py
@@ -18,15 +18,23 @@ print(zoro.name)
 print(sanji.name)
 
 #METHOD in CLASS
+#method are inside the class, but function are outside the class
 print("============METHOD in CLASS============")
+#class
 class pirates():
     name = "the name"
+    #method
+    def power(self): #self used to indentify the object
+        print(self.name,"are the sword master")
+    def power2(self, condition):
+        print(self.name,"are the invisible rope", condition)
 
+#main program
 mihawk = pirates()
 doflamingo = pirates()
 
 mihawk.name = "takanome mihawk"
 doflamingo.name = "donqixote doflamingo"
 
-print(mihawk.name)
-print(doflamingo.name)
+mihawk.power()
+doflamingo.power2("and the cruel one")#input the condition

80e60daaa119ca8ad370fbe088b3be12ceec4501
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-05-04 17:47:36 +0700

    update sample count

--- Variable_class.py
+++ Variable_class.py
@@ -2,7 +2,8 @@
 Variable Class
 '''
 
-
+#sample 1
+print("================sample 1==============")
 class employees():
     
     department = "Finance" #public variable owned by class
@@ -23,3 +24,19 @@ print(employees.department)
 
 #print the dictionary each object
 print(a.__dict__) #it will print -> name:mario, id:1001, department:system analyst
+
+#sample 2
+print("================sample 2==============")
+
+class student():
+    total_student = 0
+    def __init__(self,input_sName,input_sID):
+        self.sName = input_sName
+        self.sID = input_sID
+        
+        student.total_student +=1
+
+#main program
+x = student("benzema", 3001)
+y = student("mane", 3002)
+print(student.total_student) #it will count sum of student

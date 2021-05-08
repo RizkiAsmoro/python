76427635ed5fd1eb3ad6b6510098620090922587
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-05-08 14:24:31 +0700

    overwrite

--- Class_inheritance.py
+++ Class_inheritance.py
@@ -1,7 +1,11 @@
 '''
 CLASS INHERITANCE
+-Inheritance allows us to define a class that inherits all the methods and properties from another class.
+-Parent class is the class being inherited from, also called base class.
+-Child class is the class that inherits from another class, also called derived class.
 '''
-
+#sampe1
+print("==============Sample 1==============")
 class Parent():
     def __init__(self,name,age,addres):
         self.name = name
@@ -10,8 +14,30 @@ class Parent():
     def check (self):
         print("name :",self.name,"\nage:",self.age,"\naddres:",self.addres)
 
+class Child(Parent): #inheritance (all the object owned by Parent, owned by chid too)
+    pass
+
 jaka = Parent("Jaka",21,"Jakarta")
-kaka = Parent("kaka", 22, "Bandung")
+kaka = Child("kaka", 22, "Bandung")
+jaka.check()
+kaka.check()
+
+#sampe2 (overwrite)
+print("==============Sample 2==============")
+class Parent():
+    def __init__(self,name,age,addres):
+        self.name = name
+        self.age = age
+        self.addres = addres
+    def check (self):
+        print("name :",self.name,"\nage:",self.age,"\naddres:",self.addres)
+
+class Child(Parent): #inheritance (all the object owned by Parent, owned by chid too)
+    def check(self):
+        print("-object in parent has been overwritten")
+
+jaka = Parent("Jaka",21,"Jakarta")
+kaka = Child("kaka", 22, "Bandung")
 
 jaka.check()
 kaka.check()

6be6632ddddfcc2462a11fd3b01faad6478409cf
RizkiAsmoro <rizkiasmoro@gmail.com>
2021-05-11 15:57:06 +0700

    read and append

--- File-Handling/File_handling.py
+++ File-Handling/File_handling.py
@@ -6,6 +6,21 @@
 "r+"- write and read mode
 '''
 
+#write mode
 file = open("data.txt","w")
 file.write("This is data text, created by python")
-file.close()
+file.write("\nthis is 2nd row data text")
+file.write("\nthis is 3rd row data text")
+file.close() #need close tomakesure data.txt saved in write mode
+
+#read mode
+file2 = open("data.txt","r")
+#print(file2.read()) # it will read all content data if .read(10) its mean will read 10 character 
+print(file2.readline()) # read data by line
+file2.close()
+
+#append mode
+file3 = open("data.txt","a")
+file3.write("\nthis text made using append")
+file3.close()
+
